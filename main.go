91c8e695577af577e7d54887e5f85a33d1d1ba6c
oriversegura <oriverseguravargas@gmail.com>
2024-11-09 01:34:30 -0400

    some changes

--- main.go
+++ main.go
@@ -15,11 +15,14 @@ type Pasajero struct {
 	Destino  string `json:"Destino" validate:"required"`
 }
 
-var pasajero Pasajero
-var archivo = "pasajeros.json"
+var (
+	pasajero  Pasajero
+	archivo   = "pasajeros.json"
+	continuar = true
+	seleccion = 0
+)
 
 func main() {
-
 	// menuPrincipal : Esto imprime el menu principal y acepta un valor entero
 	// y retorna un entero con el numero de la seleccion.
 	var menu [4]string
@@ -28,39 +31,40 @@ func main() {
 	menu[2] = "Cancelacion de Vuelo"
 	menu[3] = "Disponibilidad de Asientos"
 
-	// Mostrar el menu
-	fmt.Println("Seleccione una opcion:")
-	for i, opcion := range menu {
-		fmt.Printf("%d. %s\n", i+1, opcion)
-	}
+	for continuar {
+		// Mostrar el menu
+		fmt.Println("Seleccione una opcion:")
+		for i, opcion := range menu {
+			fmt.Printf("%d. %s\n", i+1, opcion)
+		}
 
-	// Preguntar la opcion al usuario
-	var seleccion int
-	fmt.Println("Ingrese la opcion deseada: ")
-	_, err := fmt.Scanln(&seleccion)
-	if err != nil {
-		log.Fatal(err)
+		// Preguntar la opcion al usuario
+		fmt.Println("Ingrese la opcion deseada: ")
+		_, err := fmt.Scanln(&seleccion)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if seleccion < 1 || seleccion > len(menu) {
+			fmt.Println("Seleccion Invalida, intente nuevamente")
+			fmt.Println("")
+			continuar = true
+		} else {
+			continuar = false
+		}
 	}
-	if seleccion < 1 || seleccion > len(menu) {
-		fmt.Println("Seleccion Invalida")
-		return
-	}
-	fmt.Println("Usted ha seleccionado: ")
 
 	switch seleccion {
 	case 1:
 		fmt.Println(menu[0])
-		registrarPasajero(archivo)
+		err := registrarPasajero(archivo)
 		if err != nil {
 			log.Fatal(err)
 		}
-		break
 	case 2:
 		fmt.Println(menu[1])
-		break
+
 	case 3:
 		fmt.Println(menu[2])
-		break
 	case 4:
 		fmt.Println(menu[3])
 	default:
@@ -69,33 +73,35 @@ func main() {
 }
 
 func registrarPasajero(archivo string) error {
-
 	var pasajeros []Pasajero
 
 	err := revisarJSON(archivo)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	file, err := os.ReadFile(archivo)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	json.Unmarshal([]byte(file), &pasajeros)
 
 	nuevoPasajero := Pasajero{}
 
+	//Primero preguntamos el ID
 	fmt.Println("Inserte el ID de pasajero: ")
 	fmt.Scanf("%d", &nuevoPasajero.ID)
-	fmt.Println("Inserte el nombre del pasajero: ")
-	fmt.Scanf("%s", &nuevoPasajero.Nombre)
-	fmt.Println("Inserte el apellido del pasajero: ")
-	fmt.Scanf("%s", &nuevoPasajero.Apellido)
+
+	//Nombre, Apellido y Destino que sean multiple.
+	fmt.Println("Inserte el nombre del pasajero (enter para finalizar): ")
+	fmt.Scanln(&nuevoPasajero.Nombre)
+	fmt.Println("Inserte el apellido del pasajero (enter para finalizar): ")
+	fmt.Scanln(&nuevoPasajero.Apellido)
 	fmt.Println("Inserte la edad del pasajero: ")
 	fmt.Scanf("%d", &nuevoPasajero.Edad)
-	fmt.Println("Destino del pasajero: ")
-	fmt.Scanf("%s", &nuevoPasajero.Destino)
+	fmt.Println("Inserte el nombre del pasajero (enter para finalizar): ")
+	fmt.Scan(&nuevoPasajero.Destino)
 
 	pasajeros = append(pasajeros, nuevoPasajero)
 
@@ -107,7 +113,7 @@ func registrarPasajero(archivo string) error {
 
 	err = os.WriteFile(archivo, pasajeroJSON, 0644)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	fmt.Println("Pasajero registrado exitosamente!")
